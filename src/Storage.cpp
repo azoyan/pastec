befdf6cb8284980e2ee9e8d1a8f2a58503a2a30a
azoyan <azoyan.89@mail.ru>
2017-09-03 07:20:16 +0300

    storage rewrited to hashMap

--- src/Storage.cpp
+++ src/Storage.cpp
@@ -4,38 +4,35 @@
 #include <cassert>
 
 pastec::Storage::Storage(const char* dictionaryFilePath)
-: m_dictionary(CreateDictionary(dictionaryFilePath))
+  : m_dictionary(CreateDictionary(dictionaryFilePath))
 {
   assert(!m_dictionary.empty());
   m_sessions.reserve(m_dictionary.size());
   std::cout << "Init Storage\n";
 }
 
-pastec::Key pastec::Storage::insert(const Data& data, const std::chrono::seconds& lifeTime) noexcept {
-  size_t i = 0;
-  const size_t count = m_sessions.size();
-  while (i < count) {
-    Session&   session           = m_sessions.at(i);
-    const bool hasSessionExpired = session.isExpired();
-
-    if (hasSessionExpired) {
-      session.setData(data, lifeTime);
-      return session.key();
-    }
-    ++i;
+pastec::Key pastec::Storage::insert(const Data& data, const std::chrono::seconds& duration) noexcept {
+  Key key;
+  const Time now = Time::clock::now();
+  const Time expirationTime = now + duration;
+  if (auto potential = m_expireDates.crbegin(); (!m_expireDates.empty()) && (*potential).first < now) {
+    auto lifetimeNode = m_expireDates.extract((*potential).first);
+    lifetimeNode.key() = expirationTime;
+    m_expireDates.insert(std::move(lifetimeNode));
   }
-  const Key& key = m_dictionary.at(i);
-  m_sessions.emplace_back(key, data, lifeTime);
+  else {
+    key = m_dictionary.at(m_expireDates.size());
+    m_expireDates.insert({expirationTime, key});
+  }
+  m_sessions.insert_or_assign(key, data);
   return key;
 }
 
 pastec::Data pastec::Storage::data(const Key& key) const noexcept {
-  for (const Session& session : m_sessions) {
-    const bool hasKey            = session.key() == key;
-    const bool hasSessionExpired = session.isExpired();
-    if (hasKey and not hasSessionExpired) {
-      return session.data();
-    }
+  Data data;
+  auto it = m_sessions.find(key);
+  if (it != m_sessions.end()) {
+    data = it->second;
   }
-  return Data();
+  return data;
 }
